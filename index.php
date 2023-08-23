7d3546a2f50542d6319ae824182dc381a33867d7
Eros000 <e.ghilardi@hotmail.it>
2023-08-23 17:49:07 +0200

    working on it

---
+++ index.php
@@ -0,0 +1,59 @@
+
+<?php
+
+    
+    
+    function randomPassword() {
+        $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+        $pass = array(); 
+        $alphaLength = strlen($alphabet) - 1; 
+        $length = strlen($_GET["password-length"]);
+        for ($i = 0; $i < $length; $i++) {
+            $n = rand(0, $alphaLength);
+            $pass[] = $alphabet[$n];
+        }
+        return implode($pass); 
+
+        
+    }
+
+   
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Strong Password Generator</title>
+
+    <!-- BOOTSTRAP -->
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+</head>
+<body>
+
+    <h1>Password Generator </h1>
+
+    <br>
+    <br>
+
+    
+    <form action="" method="get">
+
+        <label for="">lunghezza della password</label>
+        <input  type="text" name="password-length" value="">
+        <br>
+        <br>
+        <button>Crea</button>
+
+    </form>
+
+
+</body>
+</html>
